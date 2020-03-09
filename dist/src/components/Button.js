5a830644485c13f1099058423c9edd2620d4f384
Dani Riera <daniel.riera@qatium.com>
2020-03-09 15:40:11 +0100

    Add Button component

---
+++ dist/src/components/Button.js
@@ -0,0 +1,19 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var Button = function Button() {
+  return _react.default.createElement("button", null, "dds button");
+};
+
+var _default = Button;
+exports.default = _default;
+
+//# sourceMappingURL=Button.jsx.map
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button =  () => {
+  return (
+    <button>dds button</button>
+  )
+};
+
+export default Button;
