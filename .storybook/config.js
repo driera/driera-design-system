08c75fe579fffd0f2c84e1bc16724ff6086a2748
Dani Riera <daniel.riera@qatium.com>
2020-03-13 12:59:18 +0100

    Change storybook config

---
+++ .storybook/config.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
+import 'storybook-chromatic';
+
+addDecorator(withA11y);
+addDecorator(story => (
+  <>
+    {story()}
+  </>
+));
+
+// automatically import all files ending in *.stories.js|mdx
+configure(
+  [
+    require.context('../src', false, /Intro\.stories\.mdx/),
+    require.context('../src', true, /\.stories\.(js|mdx)$/),
+  ],
+  module
+);
